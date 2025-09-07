a6f9f04a5226ec168ad1ba8317417f37effd1f00
vava408 <valentinlemaitre06@gmail.com>
2025-09-07 12:48:28 +0200

    Premiere versionde la recherche aartste et titre

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,27 @@
 document.getElementById('searchBtn').addEventListener('click', searchArtist);
 
-async function searchArtist()
-{
+async function searchArtist() {
     const query = document.getElementById('artistInput').value;
-    if(!query)
-    {
-        return
-    }
-    document.getElementById('artistInfo').innerHTML = '';
-    document.getElementById('tracks').innerHTML = '';
-    console.log(query)
+    if (!query) return;
 
-    //recherche avec l api
-    try
-    {
-        const res = await fetch(`http://localhost:3000/search?q=${encodeURIComponent(query)}`)
-        const resjson = res.json();
-        console.log(res);
-        console.log(resjson);
+    const resultsList = document.getElementById('results');
+    resultsList.querySelectorAll('li:not(.header)').forEach(li => li.remove());
+
+    try {
+        const res = await fetch(`http://localhost:3000/search?q=${encodeURIComponent(query)}`);
+        const resjson = await res.json();
+
+        resjson.data.forEach(track => {
+            const li = document.createElement('li');
+
+            li.innerHTML = `
+                <img src="${track.artist.picture}" alt="${track.artist}">
+                <span class="titre">${track.title}</span>
+                <span class="artiste">${track.artist.name}</span>
+            `;
+
+            resultsList.appendChild(li);
+        });
 
     } catch (err) {
         console.error('Erreur lors de la recherche:', err);
